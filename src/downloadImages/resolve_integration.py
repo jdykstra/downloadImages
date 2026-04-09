5d76607c176f9366f2ecd563739fcd5b69f12192
John Dykstra <john.dykstra1@gmail.com>
2026-04-09 18:25:20 -0500

    Bump Resolve launch timeout to 60 seconds.
    Version 2.0.8.

--- src/downloadImages/resolve_integration.py
+++ src/downloadImages/resolve_integration.py
@@ -19,7 +19,7 @@ def _launchResolve():
     Launch DaVinci Resolve if it's not already running and wait for it to be available.
     """
 
-    ATTEMPT_SECONDS = 30.0
+    ATTEMPT_SECONDS = 60.0
 
     while True:
         # First check if Resolve is already available
